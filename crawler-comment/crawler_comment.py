230434c41f15f10adc66024164a671d5cff00836
ngoctoan08 <toannd158@gmail.com>
2023-06-30 15:19:25 +0700

    done tt skds cand

--- crawler-comment/crawler_comment.py
+++ crawler-comment/crawler_comment.py
@@ -1,18 +1,66 @@
 ﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 from crawling_comment_vnexpress import crawling_comment_vnexpress
 from mongoengine import connect
 
 #ket noi database
-connect(db='hoc_nodejs', host='localhost', port=27017)
+#connect(db='hoc_nodejs', host='localhost', port=27017)
 
-global driver
-driver = webdriver.Chrome()
-driver.get("https://vnexpress.net/dai-tuong-to-lam-vu-dak-lak-cho-thay-khong-the-coi-thuong-an-ninh-co-so-4619672.html")
-# To load entire webpage
-time.sleep(5)
-crawlingVnexpress = crawling_comment_vnexpress(driver)
-crawlingVnexpress.crawlingComment()
-time.sleep(2)
-driver.quit()
+#global driver
+
+#driver.get("https://vnexpress.net/dai-tuong-to-lam-vu-dak-lak-cho-thay-khong-the-coi-thuong-an-ninh-co-so-4619672.html")
+## To load entire webpage
+#time.sleep(5)
+#crawlingVnexpress = crawling_comment_vnexpress(driver)
+#crawlingVnexpress.crawlingComment()
+#time.sleep(2)
+#driver.quit()
         
+
+def crawlingCommentSkds(driver):
+    url = "https://suckhoedoisong.vn/dieu-chinh-gia-nuoc-sach-o-ha-noi-phai-cung-ung-du-chat-luong-dung-quy-chuan-cua-bo-y-te-169230630093201445.htm"
+
+def crawlingCommentThanhnien(driver):
+    url = "https://thanhnien.vn/noi-lo-tri-tue-nhan-tao-tiep-quan-the-gioi-185230629122510256.htm"
+    driver.get(url)
+    time.sleep(5)
+
+    listComment = driver.find_element(By.CLASS_NAME, 'list-comment')
+    comments = listComment.find_elements(By.CLASS_NAME, 'item')
+    for comment in comments:
+        commentText = comment.find_element(By.CLASS_NAME, 'text-comment').text
+        reaction = comment.find_element(By.CLASS_NAME, 'total-like').text
+        print(commentText + '---' + reaction)
+    time.sleep(2)
+    driver.quit()
+
+#crawlingCommentThanhnien(driver)
+
+def crawlingCommentCand():
+    driver = webdriver.Chrome()
+    url = "https://cand.com.vn/Chong-dien-bien-hoa-binh/doi-tuong-nguyen-van-dai-trang-tron-xuyen-tac-vu-gay-roi-antt-o-dak-lak-i697108/"
+    driver.get(url)
+    time.sleep(10)
+    boxComment = driver.find_element(By.CLASS_NAME, 'box-comment')
+    #print(len(boxComment))
+
+    #check show more comment
+    #try:
+    #showMore = boxComment.find_elements(By.CLASS_NAME, 'label')
+    #print(len(showMore))
+    #    showMore.click()
+    #    time.sleep(3)
+    #except :
+    #    pass
+    i = 1
+    listComment = boxComment.find_element(By.TAG_NAME, 'ul')
+    comments = listComment.find_elements(By.TAG_NAME, 'li')
+    for comment in comments:
+        commentText = comment.find_element(By.TAG_NAME, 'p').text
+        #reaction = comment.find_element(By.CLASS_NAME, 'total-like').text
+        print(str(i) + commentText)
+        i = i + 1
+    time.sleep(2)
+    driver.quit()
+crawlingCommentCand()
---
+++ crawler-comment/crawling_comment_tuoitre.py
@@ -0,0 +1,73 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+from bson import ObjectId
+from models.news_comment import news_comment
+from models.news_comment import sub_comment
+
+class crawling_comment_tuoitre:
+    def __init__(self, driver):
+        self.driver = driver
+
+
+    def crawlingComment(self):
+        listComment = self.driver.find_element(By.ID, "listComment")
+        ul = listComment.find_element(By.TAG_NAME, "ul")
+        comments = ul.find_elements(By.CLASS_NAME, "item-comment")
+
+        if len(comments) <= 0:
+            print("Khong co coment nao")
+        else:
+            i = 0
+            #lặp qua từng comments thực hiện các việc sau: 
+            # lấy ra các comment chính - và comment phụ
+            for comment in comments:
+                i = i + 1
+                commentText = comment.find_element(By.CLASS_NAME, "contentcomment").text
+                elementReaction = comment.find_element(By.CLASS_NAME, "totalreact")
+                reaction = elementReaction.find_element(By.TAG_NAME, "span").text
+                # save to database
+                #commentData = news_comment(_id = ObjectId(), content=commentText, reaction=reaction)
+                #print(str(i) + commentText + '---' + reaction)
+                #sub comment
+                
+                try:
+                    showSubComment = comment.find_element(By.CLASS_NAME, 'viewreply')
+                    isSubcomment = showSubComment.get_attribute('data-visible')
+
+                    if isSubcomment == "1":
+                        print(isSubcomment)
+
+                        showSubComment.click()
+                        time.sleep(15)
+                    
+                        subCommentElement = comment.find_element(By.CLASS_NAME, 'comment-rep-top')
+                        subComments = subCommentElement.find_elements(By.CLASS_NAME, 'item-comment')
+                        print('---' + len(subComments))
+                        for subComment in subComments:
+                            subCommentText = subComment.find_element(By.CLASS_NAME, "contentcomment").text
+                            subElementReaction = subComment.find_element(By.CLASS_NAME, "totalreact")
+                            subReaction = subElementReaction.find_element(By.TAG_NAME, "span").text
+                            print('----' + subCommentText + '---' + subReaction)
+                            #luu vao database
+                            #commentData.subcomments.append(sub_comment(id=ObjectId(), content=self.getContent(subComment), reaction=self.getReaction(subComment)))
+
+                except:
+                    pass
+                #commentData.save()
+                #print(commentData.to_json())
+
+
+global driver
+driver = webdriver.Chrome()
+driver.get("https://tuoitre.vn/quoc-lo-o-tp-hcm-un-u-toi-7km-xe-co-bo-tung-chut-20230629200625329.htm")
+# To load entire webpage
+time.sleep(10)
+crawlingTuoitre = crawling_comment_tuoitre(driver)
+crawlingTuoitre.crawlingComment()
+time.sleep(2)
+driver.quit()
+
+
